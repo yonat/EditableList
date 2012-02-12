09c8d2177a8d90f409544c845a5842122970d5ce
Yonat Sharon <yonat@ootips.org>
2012-02-12 11:31:17 +0200

    Initial Commit

---
+++ List/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  List
+//
+//  Created by Yonat Sharon on 12/2/12.
+//  Copyright (c) 2012 Roy Sharon Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ List/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  List
+//
+//  Created by Yonat Sharon on 12/2/12.
+//  Copyright (c) 2012 Roy Sharon Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
