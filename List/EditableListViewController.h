8c4f6c98ebd1e207189a9849be236d5d7053c44d
Yonat Sharon <yonat@ootips.org>
2012-02-12 22:01:52 +0200

    Renamed class added NSLocalizedString for placeholder text create new text field if can't find one (because not defined in xib)

---
+++ List/EditableListViewController.h
@@ -0,0 +1,14 @@
+//
+//  EditableListViewController.h
+//
+//  Created by Yonat Sharon on 12/2/12.
+//  Copyright (c) 2012 Roy Sharon Ltd. All rights reserved.
+//
+//  Simple List, with easy editing and adding by typing the next row (a la Reminders.app)
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EditableListViewController : UITableViewController
+
+@end
